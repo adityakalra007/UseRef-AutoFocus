93348f7484e05e027ae01550c1ae0d22a0fb13ea
Aditya Kalra <aditya.kalra.cs@gmail.com>
2025-07-05 00:10:23 +0530

    Added More Functionality like reset Button

--- src/components/AutoFocus.jsx
+++ src/components/AutoFocus.jsx
@@ -3,16 +3,26 @@ import { useRef, useEffect, useState} from "react";
 export const AutoFocus = () =>{
 
         const[countdown,setCountDown] = useState(5);
+        const[showReset,setShowReset] = useState(false);
         const inputRef = useRef(null);
+        const audioRef = useRef(null);
 
         useEffect(()=>{
+            
             const timer = setTimeout(()=>{setCountDown((prev)=>prev-1)},1000);
             if(countdown===0){
                 inputRef.current.focus();
+                setShowReset(true);
                 return;
             }
             return () => clearTimeout(timer);
         },[countdown])
+
+        function handleReset(){
+            setCountDown(5);
+            inputRef.current.blur();
+            inputRef.current.value = "";
+        }
 
     return(
         <>
@@ -21,8 +31,12 @@ export const AutoFocus = () =>{
                     <h1 className="text-3xl font-semibold text-white">Focus Timer</h1>
                     <p className="text-xl text-white">{countdown >0 ? `Starting in ${countdown}...`:`Type Below`}</p>
                     <input type='text' ref={inputRef} placeholder="Type Something..." className="w-full px-4 py-3 rounded-lg bg-white/20 text-white placeholder-white/70 backdrop-blur-sm border border-white/30 focus:outline-none focus ring-2 focus:ring-purple-600 transition "></input>
+                  {showReset &&   <button onClick={handleReset} className="cursor-pointer mt-4 px-4 py-2 rounded bg-white/20 text-white font-medium hover:bg-white/30 transition border border-white/20">🔁 Reset</button>}   
                 </div>
             </div>      
+            <audio ref={audioRef}>
+                <source src="https://www.soundjay.com/button/beep-07.wav" type="audio/wav"></source>
+            </audio>
         </>
     )
 }
